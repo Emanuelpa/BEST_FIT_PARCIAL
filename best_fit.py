4ecc407741890a8e0ffed142a9f767cbb7d3d461
Emanuelpa <epatinov@eafit.edu.co>
2025-03-21 17:27:43 -0500

    SEGUNDO PARCIAL

--- best_fit.py
+++ best_fit.py
@@ -1,9 +1,3 @@
-def first_fit(memory, requeriment, index):
-    raise NotImplementedError("first_fit no está implementado aún.")
-
-def worst_fit(memory, requeriment, index):
-    raise NotImplementedError("worst_fit no está implementado aún.")
-
 def best_fit(memory, requeriment, index):
 
     if not memory:
@@ -19,6 +13,7 @@ def best_fit(memory, requeriment, index):
     n = len(memory)
     i = index % n
     less = float('inf')
+
     for _ in range(n):
         row = memory[i]
         if (row[1] >= requeriment) and (row[1] - requeriment) < less:
@@ -30,33 +25,17 @@ def best_fit(memory, requeriment, index):
     if encontered:
         selected_row = memory[index]
         print("Elige el espacio->",hex(selected_row[0]),hex(selected_row[1]))
+        print("Queda asignado en-> (" + str(hex(selected_row[0])) + ", "+ str(hex(requeriment)) + ")")
+
         if less == 0:
             print("No sobra nada de ese espacio")
             memory.pop(index)
+            index = index % len(memory)
         else:
-            print("Queda asignado en-> (" + str(selected_row[0]) + ", "+ str(requeriment) + ")")
             new_base = selected_row[0] + requeriment
             new_limit = selected_row[1] - requeriment
             updated_row = (new_base, new_limit)
             memory[index] = updated_row
-        return memory,new_base,new_limit,index
+        return memory, selected_row[0], requeriment, index
     else:
         return None
-
-
-# initial_memory = [(0, 0), (0, 0), (0,0), (0, 0)]
-# print("Memoria inicial")
-# for row in initial_memory:
-#     print(row)
-
-# memory_result,base, limit, index = best_fit(initial_memory ,100, 3)
-
-
-# if base != None:
-#     print("Nueva memoria actualizada")
-# else:
-#     print("No se encontró un espacio")
-
-# for row in memory_result:
-#     print(row)
-# print("\n\nProximo indice para iniciar busqueda", index)
